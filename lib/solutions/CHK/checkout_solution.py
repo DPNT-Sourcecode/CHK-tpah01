6e617e90622da0684904675977e90776f09f46bd
sbx_user1051 <sbx_user1051@169.254.9.213>
2024-06-10 16:17:02 +0000

    Mon Jun 10 16:17:02 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,7 +2,25 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+PRICES = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15
+}
+
+
+
 def checkout(skus):
-    basket = {}
-    raise NotImplementedError()
+    basket = {"A": 0,
+              "B": 0,
+              "C": 0,
+              "D": 0}
+    total = 0
+
+    for char in skus:
+        if char not in basket:
+            return -1
+        basket[char] += 1
+
 
