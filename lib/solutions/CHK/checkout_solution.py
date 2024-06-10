37d5ec9012949d0bc9b18991ca7e3338938a4940
sbx_user1051 <sbx_user1051@169.254.9.213>
2024-06-10 16:34:31 +0000

    Mon Jun 10 16:34:31 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,10 +15,7 @@ OFFERS = {
 }
 
 def checkout(skus):
-    basket = {"A": 0,
-              "B": 0,
-              "C": 0,
-              "D": 0}
+    basket = {"A": 0, "B": 0, "C": 0, "D": 0}
     total = 0
 
     for char in skus:
@@ -48,3 +45,4 @@ def checkout(skus):
     return total
 
 print(checkout("ABCD"))
+
