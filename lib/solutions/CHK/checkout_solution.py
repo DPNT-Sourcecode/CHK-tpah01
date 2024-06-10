60479391a1ab948fdbea32dd19fa287e87444c56
sbx_user1051 <sbx_user1051@169.254.9.213>
2024-06-10 16:20:02 +0000

    Mon Jun 10 16:20:02 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,7 +9,10 @@ PRICES = {
     "D": 15
 }
 
-
+OFFERS = {
+    "A": (3, 130),
+    "B": (2, 45)
+}
 
 def checkout(skus):
     basket = {"A": 0,
@@ -22,5 +25,13 @@ def checkout(skus):
         if char not in basket:
             return -1
         basket[char] += 1
+    
+    for sku in basket:
+        amount = basket[sku]
+        if sku in OFFERS:
+            # offer calculation
+        else:
+            total += PRICES[sku] * amount
+
 
 
